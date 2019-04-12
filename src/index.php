79b8bd95152e73139541c97828b40e0bd811b8da
AnatoliyKhaulin <haulin@e-krit.ru>
2019-04-12 21:13:53 +0300

    Разметка промо блока, данные в data.php

--- src/index.php
+++ src/index.php
@@ -10,7 +10,44 @@
 <?php
 	require('./includes/header.php');
 ?>
+<main class="wrapper">
+	<div class="promo">
+		<div class="promo__container container">
+			<div class="promo-picture">
+				<div class="good-image">
+					<img src="<?= $imgGood?>" srcset="<?= $imgX2Good?>" alt="<?= $altGood?>">
+				</div>
 
+				<div class="plants-image">
+					<img src="<?= $imgPlantsFirst?>" srcset="<?= $imgX2PlantsFirst?>" alt="<?= $altPlantsFirst?>">
+				</div>
+
+				<div class="plants-image-1">
+					<img src="<?= $imgPlantsSecond?>" srcset="<?= $imgX2PlantsSecond?>" alt="<?= $altPlantsSecond?>">
+				</div>
+
+				<footer class="promo-picture__footer">
+					<?= $promoPictureFooter?>
+				</footer>
+			</div>
+
+			<article class="promo-description">
+				<h1 class="promo-description__title"><?= $titlePromo?></h1>
+				<p class="promo-description__subtitle"><?= $subTitlePromo?></p>
+				<ul class="promo-description__list">
+					<li class="promo-description__list-item">
+						<i class="sprite-ic_clock"></i>
+						<span>сокращает продолжительность и выраженность диареи</span>
+					</li>
+					<li class="promo-description__list-item">
+						<i class="sprite-ic_drop"></i>
+						<span>восстанавливает водно-солевой баланс</span>
+					</li>
+				</ul>
+			</article>
+		</div>
+	</div>
+</main>
 <?php
 	require('./includes/footer.php');
 ?>
--- src/resources/data.php
+++ src/resources/data.php
@@ -1,5 +1,21 @@
 <?php
-  $pageTitle = 'Комплексное решение при детской диарее';
-  $pageSubTitle = 'гипоосмолярный глюкозо-солевой раствор + лактобактерии LGG';
   $headerMenuItems = array('Причины','Помощь','Применение','Преимущества','Дополнительно','Купить');
+  // Пути к изображениям
+  $imgGood = $defaultPath . 'images/sprites/img_good.png';
+  $imgX2Good = $defaultPath . 'images/sprites/img_good@2x.png';
+  $altGood = 'Изображение лекарства';
+
+  $imgPlantsFirst = $defaultPath . 'images/sprites/img_plants_1.png';
+  $imgX2PlantsFirst = $defaultPath . 'images/sprites/img_plants_1@2x.png';
+  $altPlantsFirst = 'Изображение растения';
+
+  $imgPlantsSecond = $defaultPath . 'images/sprites/img_plants_2.png';
+  $imgX2PlantsSecond = $defaultPath . 'images/sprites/img_plants_2@2x.png';
+  $altPlantsSecond = 'Изображение растения';
+  // Блок с текстом
+  $titlePromo = 'комплексное решение при детской диарее';
+  $subTitlePromo = 'гипоосмолярный глюкозо-солевой раствор + лактобактерии LGG';
+  $promoPictureFooter = "<strong>Регидрон Био</strong> – современное комбинированное средство <strong>для детей от трёх лет</strong> или с массой тела от 12 кг, для устранения и профилактики обезвоживания и облегчения диареи и рвоты";
+  $promoDescriptionFirst = 'сокращает продолжительность и выраженность диареи';
+  $promoDescriptionSecond = 'восстанавливает водно-солевой баланс';
 ?>
