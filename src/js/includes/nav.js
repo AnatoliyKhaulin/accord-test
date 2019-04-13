c681791acb2706feff6d6231c552bbfea8a637dd
AnatoliyKhaulin <haulin@e-krit.ru>
2019-04-13 15:27:11 +0300

    Анимированная линия у ссылок в навигации

--- src/js/includes/nav.js
+++ src/js/includes/nav.js
@@ -1,14 +1,8 @@
-let navLinks = document.querySelectorAll('.nav__link');
-
-for (let i = 0; i < navLinks.length; i++) {
-  let navLink = navLinks[i];
-}
-
 let menuBtn = document.querySelector('.js-burger-btn');
 
-let	menuBurger = menuBtn.querySelector('.main-header__burger');
+let menuBurger = menuBtn.querySelector('.main-header__burger');
 
-let	topHeaderNav = document.querySelector('.js-main-header-nav');
+let topHeaderNav = document.querySelector('.js-main-header-nav');
 
 let topHeaderNavLinks = topHeaderNav.querySelectorAll('.js-nav-link');
 
@@ -35,3 +29,93 @@ for (let i = 0; i < topHeaderNavLinks.length; i++) {
     }
   });
 }
+
+// Бордер у активного пункта
+
+if ($(window).width() > 768) {
+  $('.nav__list').append("<li class='nav__border-bottom'></li>");
+
+  let $magicLine = $('.nav__border-bottom');
+
+  $('.nav__item').first().addClass('nav__item--active');
+  $magicLine
+    .width($('.nav__item--active').width())
+    .css('left', $('.nav__item--active .nav__link').position().left)
+    .data('origLeft', $magicLine.position().left)
+    .data('origWidth', $magicLine.width());
+
+  $('.nav__item')
+    .find('.nav__link')
+    .hover(
+      function () {
+        let $el = $(this);
+
+        let leftPos = $el.position().left;
+
+        let newWidth = $el.parent().width();
+
+        $magicLine.stop().animate({
+          left: leftPos,
+          width: newWidth,
+        });
+      },
+
+      function () {
+        $magicLine.stop().animate({
+          left: $magicLine.data('origLeft'),
+          width: $magicLine.data('origWidth'),
+        });
+      });
+} else {
+  let $magicLine = $('.nav__border-bottom');
+
+  $magicLine.remove();
+}
+
+$(window).on('resize', function () {
+  if ($(window).width() > 768) {
+    let $magicLine = $('.nav__border-bottom');
+
+    console.log($magicLine);
+
+    if ($magicLine.length === 0) {
+      $('.nav__list').append("<li class='nav__border-bottom'></li>");
+    }
+
+
+    $('.nav__item').first().addClass('nav__item--active');
+    $magicLine
+      .width($('.nav__item--active').width())
+      .css('left', $('.nav__item--active .nav__link').position().left)
+      .data('origLeft', $magicLine.position().left)
+      .data('origWidth', $magicLine.width());
+
+    $('.nav__item')
+      .find('.nav__link')
+      .hover(
+        function () {
+          let $el = $(this);
+
+          let leftPos = $el.position().left;
+
+          let newWidth = $el.parent().width();
+
+          $magicLine.stop().animate({
+            left: leftPos,
+            width: newWidth,
+          });
+        },
+
+        function () {
+          $magicLine.stop().animate({
+            left: $magicLine.data('origLeft'),
+            width: $magicLine.data('origWidth'),
+          });
+        });
+  } else {
+    let $magicLine = $('.nav__border-bottom');
+
+    $magicLine.remove();
+  }
+});
+
--- src/resources/includes/header.php
+++ src/resources/includes/header.php
@@ -3,6 +3,7 @@
     <a href="#" class="main-header__logo">РЕГИДРОН БИО</a>
     <nav class="nav js-main-header-nav">
       <ul class="nav__list">
+        <!-- <li class="nav__border-bottom"></li> -->
         <?php foreach($headerMenuItems as $headerMenuItem) : ?>
           <li class="nav__item">
             <a href="#" class="nav__link js-nav-link"><?php echo $headerMenuItem; ?></a>
