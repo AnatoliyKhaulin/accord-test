ec141109bc7477130887b92c2d1be0b203a967c6
AnatoliyKhaulin <haulin@e-krit.ru>
2019-04-13 12:53:03 +0300

    стили адаптив, мобильное меню

--- src/js/includes/nav.js
+++ src/js/includes/nav.js
@@ -2,6 +2,36 @@ let navLinks = document.querySelectorAll('.nav__link');
 
 for (let i = 0; i < navLinks.length; i++) {
   let navLink = navLinks[i];
+}
 
-  navLinks[0].classList.add('nav__link--active');
+let menuBtn = document.querySelector('.js-burger-btn');
+
+let	menuBurger = menuBtn.querySelector('.main-header__burger');
+
+let	topHeaderNav = document.querySelector('.js-main-header-nav');
+
+let topHeaderNavLinks = topHeaderNav.querySelectorAll('.js-nav-link');
+
+menuBtn.addEventListener('click', () => {
+  if (menuBurger.classList.contains('active')) {
+    menuBurger.classList.remove('active');
+    topHeaderNav.classList.remove('active');
+  } else {
+    menuBurger.classList.add('active');
+    topHeaderNav.classList.add('active');
+  }
+});
+
+for (let i = 0; i < topHeaderNavLinks.length; i++) {
+  let navLink = topHeaderNavLinks[i];
+
+  navLink.addEventListener('click', () => {
+    if (menuBurger.classList.contains('active')) {
+      menuBurger.classList.remove('active');
+      topHeaderNav.classList.remove('active');
+    } else {
+      menuBurger.classList.add('active');
+      topHeaderNav.classList.add('active');
+    }
+  });
 }
--- src/resources/includes/header.php
+++ src/resources/includes/header.php
@@ -1,14 +1,21 @@
 <header class="main-header">
   <div class="main-header__container container">
     <a href="#" class="main-header__logo">РЕГИДРОН БИО</a>
-    <nav class="nav">
+    <nav class="nav js-main-header-nav">
       <ul class="nav__list">
         <?php foreach($headerMenuItems as $headerMenuItem) : ?>
           <li class="nav__item">
-            <a href="#" class="nav__link"><?php echo $headerMenuItem; ?></a>
+            <a href="#" class="nav__link js-nav-link"><?php echo $headerMenuItem; ?></a>
           </li>
         <?php endforeach; ?>
       </ul>
     </nav>
+    <button class="main-header__burger-btn js-burger-btn">
+      <span class="main-header__burger">
+        <span class="main-header__burger-topper"></span>
+        <span class="main-header__burger-bottom"></span>
+        <span class="main-header__burger-footer"></span>
+      </span>
+    </button>
   </div>
 </header>
